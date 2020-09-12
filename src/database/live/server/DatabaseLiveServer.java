42337b3e997adca0c57a6a854c3c2d6c2e055872
Mahadi Hassan <mithun.2121@yahoo.com>
2020-09-12 18:16:55 +0600

    commiting

---
+++ src/database/live/server/DatabaseLiveServer.java
@@ -0,0 +1,15 @@
+
+package database.live.server;
+
+import java.util.Scanner;
+
+public class DatabaseLiveServer {
+
+    public static void main(String[] args) {
+        
+        //new frm().setVisible(true);
+        new login().setVisible(true);
+        
+    }
+
+}
